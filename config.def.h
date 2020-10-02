47094934ac4728a1099fadeb7cb8c9c648a75447
Prajeen Govardhanan <prajeengovardhanan@gmail.com>
2020-10-02 16:51:41 +0530

    Applied dwm-colorbar patch from suckless.org

--- config.def.h
+++ config.def.h
@@ -13,9 +13,17 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*                      fg         bg         border */
+	[SchemeNorm]      = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]       = { col_gray4, col_cyan,  col_cyan  },
+	/* Status bar */
+	[SchemeStatus]    = { col_gray3, col_gray1, col_gray2 }, 
+	/* Tag bar */
+	[SchemeTagsSel]   = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeTagsNorm]  = { col_gray3, col_gray1, col_gray2 },
+	/* Info bar */
+	[SchemeInfoSel]   = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeInfoNorm]  = { col_gray3, col_gray1, col_gray2 },
 };
 
 /* tagging */
